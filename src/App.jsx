83b9c8ceba0e19c26b0cf78e732dcc76e3c0cebd
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-10-19 23:15:18 +0200

    project cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro/Intro';
+import Project from './components/Project/Project'
 
 function App() {
   return (
     <>
      <Navbar />
      <Intro />
+     <Project />
     </>
   );
 }
