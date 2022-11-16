98d611d4021181f5701cdee97fef66b8d392a799
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-11-16 19:48:57 +0100

    intro typewriter update

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -12,19 +12,22 @@ function Introduction() {
         <img className='Iso-img' src="assets/room_nobg.png" alt="Isometric Room"/>
       </Col>
       <Col lg={7} className='type'>
-        <TypeWriterEffect
-          startDelay={2000}
-          cursorColor="#3F3D56"
-          multiText={[
-            "This is Kyara's Portfolio",
-            'Student',
-            'Software',
-            'Developer',
-            'Kyara de Winter | Portfolio'
-          ]}
-          multiTextDelay={1000}
-          typeSpeed={30}
-      />
+        <div className='typing'>
+          <p>Kyara de Winter | Portfolio</p>
+          <TypeWriterEffect className='typewriter'
+            startDelay={1500}
+            cursorColor="#3F3D56"
+            multiText={[
+              'Software Developer',
+              'Pokemoncard Collector ',
+              'Student @ Technovium',
+              'Aquarium Keeper',
+              'Front-end Enthousiast',
+            ]}
+            multiTextDelay={1000}
+            typeSpeed={30}
+          />
+        </div>
       </Col>
     </Row>
   </Container>
